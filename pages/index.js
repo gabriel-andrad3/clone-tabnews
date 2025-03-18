d1267f824a44779045f058446b8030907aef10f3
Gabriel Andrade <gandradevini@gmail.com>
2025-03-17 23:04:25 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Vida, eu te amo. Se tu me ama, dá uma risadinha 😄 </h1>
+  return <h1>Vida, eu te amo. Se tu me ama, dá uma risadinha 😄 </h1>;
 }
 
 export default Home;
